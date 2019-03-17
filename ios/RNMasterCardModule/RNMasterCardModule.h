1317c321d1b04a7adfb52494374c87b0c8e5d46d
Erik Poort <hello@erikpoort.nl>
2019-03-17 15:52:47 +0100

    Add empty iOS project

---
+++ ios/RNMasterCardModule/RNMasterCardModule.h
@@ -0,0 +1,15 @@
+//
+//  RNMasterCardModule.h
+//  RNMasterCardModule
+//
+//  Created by Erik Poort on 17/03/2019.
+//  Copyright (c) 2019 Erik Poort. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+#import <React/RCTUtils.h>
+
+@interface RNMasterCardModule : NSObject <RCTBridgeModule>
+
+@end
---
+++ ios/RNMasterCardModule/RNMasterCardModule.m
@@ -0,0 +1,18 @@
+//
+//  RNMasterCardModule.m
+//  RNMasterCardModule
+//
+//  Created by Erik Poort on 17/03/2019.
+//  Copyright (c) 2019 Erik Poort. All rights reserved.
+//
+
+#import "RNMasterCardModule.h"
+
+static NSString *const kRejectCode = @"MasterCardModule";
+
+@implementation RNMasterCardModule {
+}
+
+RCT_EXPORT_MODULE();
+
+@end
